6a0e1a5633f92ae01ee19ebca45b9a19452bab49
Marcus Vinicius <marcus_mayme@hotmail.com>
2020-11-28 22:10:34 -0300

    PokeApi

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>  
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/guia.png">
+      <hr>
+      <h4 class="is-size-4">Pokedex</h4>
+      <input type="text" class="input is-rounded" placeholder="Buscar o pokemon pelo nome" v-model="busca">
+      <button class="button is-fullwidth is-success" id="btnBusca" @click="buscar">Buscar</button>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
+
+export default {
+  name: 'App',
+  components: {
+    Pokemon
+  },
+  data(){
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: function() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(res => {
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      });
+  },
+  methods: {
+    buscar: function() {
+       //this.filteredPokemons = this.pokemons;
+      if(this.busca == "" || this.busca == " " ) {
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => this.busca == pokemon.name);
+      }
+    }
+  },
+  computed: {
+    /*resultadoBusca: function() {
+      if(this.busca == "" || this.busca == " " ) {
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => this.busca == pokemon.name);
+      }
+    }*/ 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#btnBusca {
+  margin-top: 2%;
+}
+</style>
